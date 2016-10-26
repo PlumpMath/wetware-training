e4ef392eeb350cc2769a904846f7773b6e60ef1c
Ryan Jones <ryan.joshua.jones@gmail.com>
2016-10-25 18:26:32 -0700

    init

---
+++ Assets/Resources/Prose/white_room/sydney.ink
@@ -0,0 +1,29 @@
+== intro ==
+
+(giggling, coming up over the top of the player and peeking into their view)
+Oh, hello there! Teehee!
+
+I'm Sydney, a cognition integration assistant! I'm here to make sure you have a smooth experience integrating your synapses with wetware.
+
+(Sydney rotates upward and dramatically pantomimes somebody asking a question)
+Now, you may be asking yourself, "Sydney, why do I need an integration assistant?"
+
+(Assuming a more scholarly tone)
+Wetware forms a very tight and complex connection with your synapses.
+
+(Blackboard materializes behind sydney showing cranium with connection points)
+Every part of the brain is affected. From the frontal lobe to the hypothalamus. Teehee!
+
+Just like when you were a baby learning how to first interact with the world, you have to relearn how to use your senses now that they are coupled with wetware.
+
+(Blackboard disappears back up and Sydney comes up close to the player as if sharing a secret)
+Now comes my favorite part! showing you the first hologram!
+
+(Sydney backs up to "official" distance and resumes scholarly tone)
+Our first step is to teach you how to make a hologram.
+
+(Player experiences first visual glitch and is subjected to a nausea-inducing distortion)
+(Sydney bounces up in alarm but remains cheerful)
+Oh my! It looks like you had an adverse reaction there! Nothing to worry about! Teehee.
+
+Now where were we? Ah yes! Creating your first hologram!
---
+++ Assets/game/core.clj
@@ -0,0 +1,101 @@
+(ns game.core
+ (require [arcadia.core :as a]
+          [arcadia.linear :as l]
+          [tween.core :refer :all])
+ (import [UnityEngine Animator AudioSource Resources Quaternion Renderer Color]))
+
+(defn emote [emotion wait-duration]
+ (let [syd-bod (a/object-named "sydneyBody")
+       syd (a/object-named "sydney")
+       orig-pos (.. syd transform position)
+       orig-color (.color (.material (a/cmpt syd-bod Renderer)))]
+  (a/set-state! syd :moving true)
+  (cond
+   (= emotion :aside)
+   (timeline*
+    (AND
+     (tween {:position (l/v3+ orig-pos (l/v3 0 0.5 0))} syd 1 {:in :pow3 :out :pow3})
+     (tween {:local {:position (l/v3 0 0 -1)}} syd-bod 1 {:in :pow3 :out :pow3})
+     (tween {:material {:color (Color. 0.8 0.2 0.2 1)}} syd-bod 1 {:in :pow3 :out :pow3}))
+    (wait wait-duration)
+    (AND
+     (tween {:position orig-pos} syd 1 {:in :pow3 :out :pow3})
+     (tween {:local {:position (l/v3 0 0 1)}} syd-bod 1 {:in :pow3 :out :pow3})
+     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3}))
+    #(a/set-state! syd :moving false))
+   (= emotion :pantomime)
+   (timeline*
+    (AND
+     (tween {:position (l/v3+ (l/v3 0 1 0) orig-pos)} syd 1 {:in :pow3 :out :pow3})
+     (tween {:material {:color (Color. 0.6 0.6 0.2 1)}} syd-bod 1 {:in :pow3 :out :pow3}))
+    (wait wait-duration)
+    (AND
+     (tween {:position orig-pos} syd 1 {:in :pow3 :out :pow3})
+     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3}))
+    #(a/set-state! syd :moving false)))))
+   
+(defn sydney-hover []
+ (let [syd-bod (a/object-named "sydneyBody")
+       syd (a/object-named "sydney")]
+  (timeline* :loop
+   #(a/state syd :moving)
+   (AND
+    (NOT #(a/state syd :moving))
+    (OR (tween {:local {:position (l/v3 0 0.2 0)}} syd-bod 1)
+        (tween {:local {:position (l/v3 0 -0.2 0)}} syd-bod 1))))))
+
+(defn sydney-entrance []
+ (let [syd (a/object-named "sydney")]
+  (timeline
+   [#(do (a/set-state! syd :moving true) false)
+     (tween {:position (l/v3 0 6 0)} syd 2 {:in :pow3 :out :pow3})
+     (tween {:position (l/v3 0 5 2)} syd 2 {:in :pow2 :out :pow2})
+     (tween {:position (l/v3 0 8 2)} syd 1 {:in :pow4 :out :pow4})
+     (tween {:position (l/v3 0 2 4)} syd 3 {:in :pow3 :out :pow3})
+     #(do (a/set-state! syd :moving false) false)])))
+
+(defn turn-on-sign []
+ (let [sign-anim (a/cmpt (a/object-named "main_sign") Animator)]
+  (.SetTrigger sign-anim "turn_on_sign")
+  (.Play (a/cmpt (a/object-named "neon_on") AudioSource))))
+
+(defn remove-diagram []
+ (let [diagram (a/object-named "diagram")
+       syd (a/object-named "sydney")]
+  (timeline*
+   (AND
+    (tween {:position (l/v3 0 10 5)} diagram 1.5 {:in :pow3})
+    (tween {:position (l/v3 0 2 4)} syd 2 {:in :pow3 :out :pow3}))
+   (tween {:scale (l/v3 0 0 0)} diagram 0.5)
+   #(do (a/destroy diagram) false))))
+
+(defn spawn-diagram []
+ (let [prefab (Resources/Load "Prefabs/brain_diagram")
+       diagram (UnityEngine.Object/Instantiate prefab (l/v3 0 10 5) Quaternion/identity)
+       syd (a/object-named "sydney")]
+  (set! (.name diagram) "diagram")
+  (timeline [(tween {:position (l/v3 0 2.3 5)} diagram 3 {:out :pow3})])
+  (timeline [(wait 2)
+             (tween {:position (l/v3 2 2.3 4)} syd 2 {:in :pow3 :out :pow3})])))
+
+(defn sydney-input []
+ (a/hook+ (a/object-named "sydney") :on-pointer-enter
+  (fn [go ptr]
+   (a/log "bork"))))
+       
+(defn init-scene [_]
+ (sydney-input)
+ (sydney-hover)
+ (sydney-entrance)
+ (timeline
+  [(wait 3.0)
+   #(do (turn-on-sign) false)
+   (wait 8.0)
+   #(do (spawn-diagram) false)
+   (wait 8.0)
+   #(do (remove-diagram) false)
+   (wait 5.0)
+   #(do (emote :aside 2) false)
+   (wait 7.0)
+   #(do (emote :pantomime 2) false)]))
+ 
