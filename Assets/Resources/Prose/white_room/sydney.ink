ab1aa368375634b1ec69ea7595e3fd37b0c8237a
Ryan Jones <ryan.joshua.jones@gmail.com>
2016-10-28 14:33:07 -0700

    on part with play store release

--- Assets/Resources/Prose/white_room/sydney.ink
+++ Assets/Resources/Prose/white_room/sydney.ink
@@ -1,7 +1,7 @@
 == intro ==
 
 (giggling, coming up over the top of the player and peeking into their view)
-Oh, hello there! Teehee!
+<Clears throat> Oh! hello there! Teehee!
 
 I'm Sydney, a cognition integration assistant! I'm here to make sure you have a smooth experience integrating your synapses with wetware.
 
@@ -17,13 +17,44 @@ Every part of the brain is affected. From the frontal lobe to the hypothalamus. 
 Just like when you were a baby learning how to first interact with the world, you have to relearn how to use your senses now that they are coupled with wetware.
 
 (Blackboard disappears back up and Sydney comes up close to the player as if sharing a secret)
-Now comes my favorite part! showing you the first hologram!
+Now comes my favorite part! Activating your interface! 
 
 (Sydney backs up to "official" distance and resumes scholarly tone)
-Our first step is to teach you how to make a hologram.
+Our first step is to teach you how to use your new wetware interface.
 
 (Player experiences first visual glitch and is subjected to a nausea-inducing distortion)
 (Sydney bounces up in alarm but remains cheerful)
 Oh my! It looks like you had an adverse reaction there! Nothing to worry about! Teehee.
 
-Now where were we? Ah yes! Creating your first hologram!
+Now where were we? Ah yes! Activating your interface!
+
+(Menu opens up in front of player, like the Hololens menu)
+This is where you can access your basic -
+
+(More distortion)
+Oh my, more interference. Not a worry! Let's just move on, shall we?
+
+(Menu clses)
+Now, the next step in the wetware install process is to make a fresh install of your memories! Teehee. Don't worry, it doesn't hurt.
+
+(Legalese sounding, like the hurried speech after a car commercial)
+By remaining conscious, you agree to have your memory backed up on our cloud storage and wiped.
+
+(Back to chipper tone)
+Great! You agree to the memory wipe. To proceed I need you to look squarely at me. <minor pause> Great!
+
+(Biff materializes off screen to the left)
+My associate, Biff, will now perform the memory wipe procedure. Please look to your left.
+
+(Biff activates in a horrifying way)
+(Sydney, still cheerful, offsceen)
+Have a good day!
+
+(Biffrushes the player, sending the screen black)
+
+(After a few seconds of breathing sound, Sydney is now off-screen talking to Biff, losing all pretense of fake cheer)
+Did you get the brain data?
+<Biff does a beeping sound>
+Great, you useless lunk. Drag this one off to the pit.
+
+<demo ends>
--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -4,15 +4,28 @@
           [tween.core :refer :all]
           [game.ui :as ui]
           [game.glitch :as glitch])
- (import [UnityEngine Animator AudioSource Resources Quaternion Renderer Color RectTransform]))
+ (import [UnityEngine Animator AudioSource Resources Quaternion Renderer Color RectTransform Camera Application]))
 
 (defn emote [emotion wait-duration]
  (let [syd-bod (a/object-named "sydneyBody")
        syd (a/object-named "sydney")
        orig-pos (.. syd transform position)
        orig-color (.color (.material (a/cmpt syd-bod Renderer)))]
+  (timeline
+   [#(do (a/set-state! syd :moving true) false)
+    (wait (+ 2 wait-duration))
+    #(do (a/set-state! syd :moving false) false)])
   (a/set-state! syd :moving true)
   (cond
+   (= emotion :greet)
+   (timeline*
+    (AND
+     (tween {:position (l/v3+ orig-pos (l/v3 0 0.5 0))} syd 1 {:in :pow3 :out :pow3})
+     (tween {:material {:color (Color. 0 0 1 1)}} syd-bod 1 {:in :pow3 :out :pow3}))
+    (wait wait-duration)
+    (AND
+     (tween {:position orig-pos} syd 1 {:in :pow3 :out :pow3})
+     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3})))
    (= emotion :aside)
    (timeline*
     (AND
@@ -23,8 +36,7 @@
     (AND
      (tween {:position orig-pos} syd 1 {:in :pow3 :out :pow3})
      (tween {:local {:position (l/v3 0 0 1)}} syd-bod 1 {:in :pow3 :out :pow3})
-     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3}))
-    #(a/set-state! syd :moving false))
+     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3})))
    (= emotion :pantomime)
    (timeline*
     (AND
@@ -33,8 +45,7 @@
     (wait wait-duration)
     (AND
      (tween {:position orig-pos} syd 1 {:in :pow3 :out :pow3})
-     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3}))
-    #(a/set-state! syd :moving false)))))
+     (tween {:material {:color orig-color}} syd-bod 1 {:in :pow3 :out :pow3}))))))
    
 (defn sydney-hover []
  (let [syd-bod (a/object-named "sydneyBody")
@@ -80,30 +91,118 @@
   (timeline [(wait 2)
              (tween {:position (l/v3 2 2.3 4)} syd 2 {:in :pow3 :out :pow3})])))
 
+(defn spawn-biff []
+ (let [prefab (Resources/Load "Prefabs/biff")
+       biff (UnityEngine.Object/Instantiate
+             prefab
+             (.. (a/object-named "biffPos") transform position)
+             (.. (a/object-named "biffPos") transform rotation))]
+  (set! (.name biff) "biff")
+  (a/hook+ biff :update
+   (fn [go]
+    (.LookAt (.transform biff) (.. Camera/main transform position))))))
+
+(defn biff-charge []
+ (let [biff (a/object-named "biff")]
+  (timeline*
+   (tween {:position (l/v3+ (l/v3* (.. Camera/main transform forward) 1.5) (.. Camera/main transform position))} biff 1 {:in :pow2 :out :pow3}))))
+
+(defn biff-jump []
+ (let [biff (a/object-named "biff")
+       biff-bod (a/object-named "biffBody")
+       orig-pos (.. biff transform position)]
+  (timeline*
+   (AND
+    (tween {:position (l/v3+ orig-pos (l/v3 0 3 0))} biff 0.5 {:out :pow3})
+    (tween {:material {:color (Color. 1 0 0 1)}} biff-bod 2 {:in :pow3 :out :pow3})))))
+
+(defn go-black []
+ (let [quad (a/cmpt (a/object-named "Quad") Renderer)]
+  (set! (.enabled quad) true)))
+
+(defn play [go]
+ (let [audio (a/cmpt go AudioSource)]
+  (.Play audio)))
+
+(defn stop [go]
+ (let [audio (a/cmpt go AudioSource)]
+  (.Stop audio)))
+
 (defn init-scene [_]
  (sydney-hover)
  (sydney-entrance)
  (timeline
   [(wait 3.0)
    #(do (turn-on-sign) false)
-   (wait 8.0)
-   #(do (spawn-diagram) false)
-   (wait 8.0)
-   #(do (remove-diagram) false)
-   (wait 5.0)
-   #(do (emote :aside 2) false)
-   (wait 7.0)
+   #(do (play (a/object-named "intro")) false)  
+   (wait 6.0)
+   #(do (emote :greet 4) false)
+   (wait 14.0)
    #(do (emote :pantomime 2) false)
-   (wait 7.0)
+   (wait 4.0)
+   #(do (play (a/object-named "education")) false)
+   (wait 2.0)
+   #(do (spawn-diagram) false)
+   (wait 16.0)
+   #(do (remove-diagram) false)
+   (wait 3.0)
+   #(do (play (a/object-named "interface_intro")) false)
+   (wait 1.0)
+   #(do (emote :aside 2) false)
+   (wait 8.0)
+   #(do (play (a/object-named "distortion1")) false)
+   #(do (play (a/object-named "distortion")) false)
+   #(do (glitch/set-depth-clearing :nothing) false)
+   (wait 4.0)
+   #(do (glitch/set-depth-clearing :skybox) false)
+   #(do (stop (a/object-named "distortion")) false)
+   (wait 4.0)
    #(do (ui/add-interface) false)
    (wait 1)
    #(do (ui/add-button (fn [])) false)
    (wait 0.5)
    #(do (ui/add-button (fn [])) false)
-   (wait 7.0)
-   #(do (ui/remove-interface) false)
+   (wait 1.0)
+   #(do (play (a/object-named "close_interface")) false)
    (wait 3.0)
+   #(do (play (a/object-named "distortion")) false)
    #(do (glitch/set-depth-clearing :nothing) false)
-   (wait 3.0)
-   #(do (glitch/set-depth-clearing :skybox) false)]))
+   (wait 2.0)
+   #(do (glitch/set-depth-clearing :skybox) false)
+   #(do (stop (a/object-named "distortion")) false)
+   (wait 4.0)
+   #(do (ui/remove-interface) false)
+   (wait 2.0)
+   #(do (play (a/object-named "memory_backup_conditions")) false)
+   (wait 5)
+   #(do (emote :pantomime 4.0) false)
+   (wait 7)
+   #(do (emote :greet 4.0) false)
+   (wait 4)
+   #(do (play (a/object-named "memory_wipe_continue")) false)
+   (wait 6)
+   #(do (spawn-biff) false)
+   (wait 8)
+   #(do (play (a/object-named "oh_shit")) false)
+   (wait 2)
+   #(do (play (a/object-named "biff_activate")) false)
+   #(do (biff-jump) false)
+   (wait 3)
+   #(do (play (a/object-named "good_day")) false)
+   (wait 2)
+   #(do (biff-charge) false)
+   #(do (play (a/object-named "biff_charge")) false)
+   (wait 0.5)
+   #(do (go-black) false)
+   #(do (play (a/object-named "hit")) false)
+   #(do (play (a/object-named "breathing")) false)
+   (wait 3)
+   #(do (play (a/object-named "get_data")) false)
+   (wait 2)
+   #(do (play (a/object-named "biff_ok")) false)
+   (wait 1.5)
+   #(do (play (a/object-named "drag")) false)
+   (wait 8.0)
+   #(do (Application/Quit) false)]))
+   
  
